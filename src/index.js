47a96eebdf0d7b487c9291de5e13f639b10ad369
Steve Kamanke <pstevek@gmail.com>
2020-03-07 15:30:50 +0200

    set AWS credentials and EC2 Object

--- src/index.js
+++ src/index.js
@@ -7,7 +7,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/create-environment', (req, res) => {
-   
+    // Get Request form data
+    const data = {
+        accessKeyId: req.body.accessKeyId,
+        secretAccessKey: req.body.secretAccessKey,
+        region: req.body.region,
+        ec2Key: req.body.accessKeyName
+    };
+
+    // Set AWS Credentials
+    aws.config.update({
+        accessKeyId: data.accessKeyId,
+        secretAccessKey: data.secretAccessKey,
+        region: data.region
+    });
+
+    // Create EC2 service object
+    const ec2 = new aws.EC2({apiVersion: '2016-11-15'});
 });
 
 
