09a0efbe0e950983d6c7bb5e95140d4d3aa116ad
Steve Kamanke <pstevek@gmail.com>
2020-03-08 00:14:45 +0200

    environment first attempt

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const   express = require('express'),
-        aws = require('aws-sdk'),
+        aws = require('aws-sdk')
         bodyParser = require('body-parser'),
         app = express();
 
@@ -12,7 +12,7 @@ app.post('/create-environment', (req, res) => {
         accessKeyId: req.body.accessKeyId,
         secretAccessKey: req.body.secretAccessKey,
         region: req.body.region,
-        ec2Key: req.body.accessKeyName
+        ec2KeyPair: req.body.ec2KeyPair
     };
 
     // Set AWS Credentials
@@ -24,7 +24,100 @@ app.post('/create-environment', (req, res) => {
 
     // Create EC2 service object
     const ec2 = new aws.EC2({apiVersion: '2016-11-15'});
-});
 
+    // Create promise on an EC2 instance
+    const vpcPromise = ec2.createVpc({CidrBlock: "10.0.0.0/16"}).promise();
+
+    vpcPromise.then(
+        function(data) {
+            vpcId = data.Vpc.VpcId;
+            const paramsSecurityGroup = {
+                Description: 'Security Group for EC2 instance',
+                GroupName: 'roam-sg-demo',
+                VpcId: vpcId
+            };
+
+            const sgPromise = ec2.createSecurityGroup(paramsSecurityGroup).promise();
+
+            sgPromise.then(
+                function(data) {
+                    const SecurityGroupId = data.GroupId;
+                    console.log("Security Group successfully created ! GroupId : ", SecurityGroupId);
+                    const paramsIngress = {
+                        GroupId: SecurityGroupId,
+                        IpPermissions: [
+                            {
+                                IpProtocol: "tcp",
+                                FromPort: 80,
+                                ToPort: 80,
+                                IpRanges: [{"CidrIp":"0.0.0.0/0"}]
+                            },
+                            {
+                                IpProtocol: "tcp",
+                                FromPort: 22,
+                                ToPort: 22,
+                                IpRanges: [{"CidrIp":"0.0.0.0/0"}]
+                            }
+                        ]
+                    };
+                    const sgIngress = ec2.authorizeSecurityGroupIngress(paramsIngress).promise();
+
+                    sgIngress.then(
+                        function(data) {
+                            console.log("Ingress Successfully Set : ", data);
+                        }
+                    ).catch(
+                        function(err) {
+                            console.error(err, err.stack);
+                        });
+                }
+            ).catch(
+                function(err) {
+                    console.error(err, err.stack);
+                });
+        }
+    ).catch(
+        function(err) {
+            console.error(err, err.stack);
+        });
+
+    const instanceParams = {
+        ImageId: 'ami-0a887e401f7654935',
+        InstanceType: 't2.micro',
+        KeyName: data.ec2KeyPair,
+        MinCount: 1,
+        MaxCount: 1,
+    };
+    
+    // Run the EC2 instance
+    const instancePromise = ec2.runInstances(instanceParams).promise();
+
+    // Handle promise's fulfilled/rejected states
+    instancePromise.then(
+        function(data) {
+            var instanceId = data.Instances[0].InstanceId;
+            console.log("Created EC2 instance : ", instanceId);
+            // Add tags to the instance
+            tagParams = {Resources: [instanceId], Tags: [
+                {
+                    Key: 'Name',
+                    Value: 'Ringier EC2 Demo 2'
+                }
+            ]};
+            // Create a promise on an EC2 service object
+            var tagPromise = ec2.createTags(tagParams).promise();
+            // Handle promise's fulfilled/rejected states
+            tagPromise.then(
+                function(data) {
+                    console.log("EC2 Instance tagged");
+                }).catch(
+                    function(err) {
+                        console.error(err, err.stack);
+                    });
+        }).catch(
+            function(err) {
+                console.error(err, err.stack);
+            });
+});
 
 module.exports = app;
